bff0f041fec2b4bc9782c02ec3e8e2eb36d83c5a
M1Deavon <m1deavon@gmail.com>
2018-10-18 16:17:02 +0300

    добавил маяки навигации
    добавил маяки навигации, создал папку js разместил файл.js в соответствующей директории

--- markup/index.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $("#navigation").on("click","a", function (event) {
-        event.preventDefault();
-        var id  = $(this).attr('href'),
-            top = $(id).offset().top;
-        $('body,html').animate({scrollTop: top}, 800);
-    });
-});
