d2b96fda314aa4d4078d65efee84d00f61cb5cbc
M1Deavon <m1deavon@gmail.com>
2018-10-25 01:26:50 +0300

    корректировка стилей для блока services
    убрал лишние комментарии, добавил к блокам контейнера анимацию отображения содержимого с задержкой при первиной загрузке страницы.

---
+++ markup/js/index2.js
@@ -0,0 +1,25 @@
+var elShowed = false;
+function callback(entry) {
+
+  if (entry.length > 0 && !elShowed) {
+    var container = entry[0];
+    var items = container.target.children;
+		var ratio = Math.floor(container.intersectionRatio);
+		
+    if (ratio > 0) {
+    	for (let i = 0; i < items.length; i++) {
+      	items[i].className += (' ' + items[i].className + '-' + i);
+      }
+      elShowed = !elShowed;
+    } 
+  }
+}
+
+var config = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 1.0
+};
+
+var observer = new IntersectionObserver(callback, config);
+observer.observe(document.querySelector('#services'));
